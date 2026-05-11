9c24f6c90b66f060ecc56f0c85aa2fea34b15477
Devikasomireddy <somireddydevika589@gmail.com>
2026-05-11 21:42:37 +0530

    Added missing Java file

---
+++ oopsinheritance/src/com/ists/oopsinheritance/Animal.java
@@ -0,0 +1,14 @@
+package com.ists.oopsinheritance;
+//Single inheritance
+public class Animal {
+	
+	public Animal()
+	{
+		System.out.println("The parent class Constructor");
+	}
+
+	public void eat()
+	{
+		System.out.println("The animal is eating......");
+	}
+}
